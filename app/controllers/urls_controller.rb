d7c074e1812be7665cc409cd8cc58ba1f0f85daf
Xavier Priour <xavier.priour@bubblyware.com>
2015-02-18 00:39:08 +0100

    enh: gracefully handle show/:incorrect_id

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,5 @@
 class UrlsController < ApplicationController
-  before_action :set_url, only: [:show, :edit, :update, :destroy]
+  before_action :set_url, only: [:show, :destroy]
 
   # GET /urls
   # GET /urls.json
@@ -10,6 +10,12 @@ class UrlsController < ApplicationController
   # GET /urls/1
   # GET /urls/1.json
   def show
+    if(@url.nil?)
+      respond_to do |format|
+        format.html { redirect_to urls_url, notice: 'Incorrect URL ID.' }
+        format.json { head :no_content }
+      end
+    end
   end
 
   # GET /urls/new
@@ -21,14 +27,14 @@ class UrlsController < ApplicationController
   # POST /urls.json
   def create
     # check if there is already an Url for these params
-    @url = Url.where(long: url_params['long']).first
+    @url = Url.where(long: url_params[:long]).first
     if (@url.nil?)
       # TODO check if the long URL is a valid one
       @url = Url.new(url_params)
     end
     respond_to do |format|
       if @url.id || @url.save
-        format.html { redirect_to @url, notice: 'Your URL was successfully shortened.' }
+        format.html { redirect_to @url, notice: 'Your URL was successfully transformed.' }
         format.json { render :show, status: :created, location: @url }
       else
         format.html { render :new }
@@ -40,15 +46,17 @@ class UrlsController < ApplicationController
   # DELETE /urls/1
   # DELETE /urls/1.json
   def destroy
-    @url.destroy
+    if (!@url.nil?)
+      @url.destroy
+    end # if not we silently ignore
     respond_to do |format|
-      format.html { redirect_to urls_url, notice: 'Url was successfully destroyed.' }
+      format.html { redirect_to urls_url, notice: 'The URL was deleted.' }
       format.json { head :no_content }
     end
   end
 
   def go
-    @url = Url.where("short = :short", params).first
+    @url = Url.where(short: params[:short]).first
     if (@url.nil?)
       # if incorrect short url, send back to main url
       redirect_to urls_url
@@ -60,8 +68,12 @@ class UrlsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_url
-      @url = Url.find(params[:id])
-      @url.full = go_urls_url(@url.short)
+      # we use where instead of find(id) because we want to handle
+      # incorrect ids gracefully.
+      @url = Url.where(id: params[:id]).first
+      if (!@url.nil?)
+        @url.full = go_urls_url(@url.short)
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
